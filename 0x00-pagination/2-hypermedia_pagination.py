84b8d4995b423768d5dee3f18eed0c4c734211b4
NateMachoka <natemachoka@gmail.com>
2024-10-28 10:34:55 -0700

    fixed pycode character length issue

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -62,7 +62,9 @@ class Server:
         return dataset[start_index:end_index] if start_index < len(dataset) \
             else []
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Optional[int]]:
+    def get_hyper(
+            self, page: int = 1, page_size: int = 10
+    ) -> Dict[str, Optional[int]]:
         """
         Returns a dictionary with pagination information.
         Parameters:
