4491ad1329476d3884d9999a67ccf88fb5a4528b
NateMachoka <natemachoka@gmail.com>
2024-10-30 05:39:47 -0700

    fixed indentation issues

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,13 +5,13 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-     """ BasicCache defines a caching system with no limit """
+    """ BasicCache defines a caching system with no limit """
 
-     def put(self, key, item):
-         """ Add an item in the cache """
-         if key is not None and item is not None:
-             self.cache_data[key] = item
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
 
-     def get(self, key):
-         """ Get an item by key """
-         return self.cache_data.get(key, None)
+    def get(self, key):
+        """ Get an item by key """
+        return self.cache_data.get(key, None)
