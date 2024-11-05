b192fa4e68a0a4f8efbf8c9d93eaccdc12359f0d
NateMachoka <natemachoka@gmail.com>
2024-11-05 08:00:58 -0800

    0-app.py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app setup for a single route that renders a simple HTML page.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index() -> str:
+    """
+    Route to render the index page.
+    Returns:
+        str: Rendered HTML template for the index page.
+    """
+    return render_template('index.html')
+
+
+if __name__ = '__main__':
+    app.run(host='0.0.0.0', port=5000)
